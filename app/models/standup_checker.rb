16586f7f4b721ca63c9a1020e6ad7a5929f93a97
Prokop Simek <prokop.simek@applifting.cz>
2016-02-10 02:37:09 +0100

    set logic of notification

--- app/models/standup_checker.rb
+++ app/models/standup_checker.rb
@@ -4,8 +4,16 @@ class StandupChecker
 
   class << self
 
+    def get_who_should_write
+      ENV.select{ |var| var.include?('STANDUP_USER') }.map{|k,v| v}
+    end
+
+    def get_whom_to_nofity_result
+      ENV.select{ |var| var.include?('STANDUP_NOTIFY') }.map{|k,v| v}
+    end
+
   def process!
-    should_write = ENV.select{ |var| var.include?('STANDUP_USER') }.map{|k,v| v}
+    should_write = get_who_should_write
 
     who_wrotes = get_who_wrote
     didnt_wrote = should_write - who_wrotes
@@ -14,7 +22,7 @@ class StandupChecker
   end
 
   def get_who_wrote
-    history = SlackClient.new.groups_history(channel: STANDUP_CHANNEL, oldest: to_timestamp(DateTime.now - 1.minutes))
+    history = SlackClient.new.groups_history(channel: STANDUP_CHANNEL, oldest: to_timestamp(DateTime.now - 21.hours)) # day before at 9pm
     messages = history['messages']
     if messages.present?
       users_who_wrote = messages.map{ |msg| msg['user'] }.uniq
@@ -29,39 +37,42 @@ class StandupChecker
       buzz!(user_id)
     end
 
-    if didnt_wrote.any?
-      notify_about_all_who_didnt_wrote(didnt_wrote)
-    else
-      notify_that_all_wrote
+    get_whom_to_nofity_result.each do |whom_id|
+      if didnt_wrote.any?
+        notify_about_all_who_didnt_wrote(didnt_wrote, whom_id)
+      else
+        notify_that_all_wrote(whom_id)
+      end
     end
   end
 
   def buzz!(user_id)
     SlackClient.new.chat_postMessage(
-      channel: STANDUP_CHANNEL,
+      channel: user_id,
       text: "Hey, <@#{user_id}>! Ty jsi za včerejšek nenapsal standup!",
       username: 'Standup checker',
       icon_url: ENV['STANDUP_BOT_ICON_URL']
     )
   end
 
-  def notify_about_all_who_didnt_wrote(didnt_wrote)
+  def notify_about_all_who_didnt_wrote(didnt_wrote, whom_id)
     names = ''
     didnt_wrote.each_with_index do |user_id, i|
       names += ', ' if i != 0
       names += "<@#{user_id}>"
     end
-    SlackClient.new.chat_postMessage(
-      channel: STANDUP_CHANNEL,
-      text: "Za včerejšek nenapsali standup tito lidé: #{names}!",
-      username: 'Standup checker',
-      icon_url: ENV['STANDUP_BOT_ICON_URL']
-    )
+
+      SlackClient.new.chat_postMessage(
+        channel: whom_id,
+        text: "Za včerejšek nenapsali standup tito lidé: #{names}!",
+        username: 'Standup bitch',
+        icon_url: ENV['STANDUP_BOT_ICON_URL']
+      )
   end
 
-  def notify_that_all_wrote
+  def notify_that_all_wrote(whom_id)
     SlackClient.new.chat_postMessage(
-      channel: STANDUP_CHANNEL,
+      channel: whom_id,
       text: "Za včerejšek napsali standup všichni lidé, od kterých to je vyžadováno.",
       username: 'Standup checker',
       icon_url: ENV['STANDUP_BOT_ICON_URL']
